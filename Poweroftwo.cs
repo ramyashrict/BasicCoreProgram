729672772778ca5e7e787c7f361dc7371ef6b39d
ramyashrict <ramyarr1415@gmail.com>
2023-03-22 02:33:32 +0530

    BasicCorePrograms

---
+++ Poweroftwo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicConceptPrograms
+{
+    internal class Poweroftwo
+    {
+    }
+    public static void FindPoweroftwo()
+    {
+        int e;
+        int result;
+
+        for (int i = 0; i < 10; i++)
+        {
+            result = 1;
+            e = i;
+
+            while (e > 0)
+            {
+                result *= 2;
+                e--;
+            }
+
+            Console.WriteLine("2 to the " + i + " power is " + result);
+        }
+    }
+}
+
